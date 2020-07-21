ce8bc38da50319200ce6dabe9790e1c3c6e8fe91
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-07-21 12:47:07 -0400

    added more sounds to midi

--- client/src/components/Midi.js
+++ client/src/components/Midi.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import useSound from 'use-sound';
 import baseloop from './sounds/baseloop.wav';
-// import disco from './sounds/disco.mp3';ž
+import kicks from './sounds/kicks.wav';
+import virgo from './sounds/virgo.wav';
+import wave from './sounds/wave.mp3';
+
 
 const useKeyboardBindings = map => {
     React.useEffect(() => {
@@ -24,20 +27,48 @@ const useKeyboardBindings = map => {
 
 
 const Midi = () => { 
-    const [play] = useSound(baseloop, {
+    let [play] = useSound(baseloop, {
         sprite: {
             kick: [0, 1800],
             hihat: [1800, 2100]
           },
     });
 
+    let [k] = useSound(kicks, {
+      sprite: {
+        kicks1: [0, 6000],
+        kicks2: [0, 6000]
+      },
+    });
+
+    let [v] = useSound(virgo, {
+      sprite: {
+        virgo1: [0, 500],
+        virgo2: [500, 900],
+        virgo3: [1200, 1250],
+      },
+    });
+
+    let [w] = useSound(wave, {
+      sprite: {
+        wave1: [0, 3000],
+        wave2: [3000, 6000],
+      },
+    });
+
+    
 
 
     useKeyboardBindings({
         1: () => play({ id: 'kick' }),
         2: () => play({ id: 'hihat' }),
-        3: () => play({ id: 'drum1' }),
-        4: () => play({ id: 'drum2' }),
+        3: () => k({ id: 'kicks1' }),
+        4: () => k({ id: 'kicks2' }),
+        5: () => v({ id: 'virgo1' }),
+        6: () => v({ id: 'virgo2' }),
+        7: () => v({ id: 'virgo3' }),
+        8: () => w({ id: 'wave1' }),
+        9: () => w({ id: 'wave2' })
       });
 
     // useKeyboardBindings({
@@ -52,34 +83,15 @@ const Midi = () => {
                     <button arian-label="kick" onMouseDown={() => play({id: 'kick'})} className="square">1</button>
                     
                     <button arian-label="hihat" onMouseDown={() => play({id: 'hihat'})} className="square">2</button>
-                    <button className="square">3</button>
+                    <button arian-label="snare" onMouseDown={() => k({id: 'kicks1'})} className="square">3</button>
                     
-                    <button arian-label="snare" onMouseDown={() => play({id: 'drum1'})} className="square">4</button>
-                    <button arian-label="cowbell" onMouseDown={() => play({id: 'drum2'})} className="square">5</button>
+                    <button arian-label="snare" onMouseDown={() => k({id: 'kicks2'})} className="square">4</button>
+                    <button arian-label="cowbell" onMouseDown={() => v({id: 'virgo1'})} className="square">5</button>
                     </>
-                    <button className="square">6</button>
-                    <button className="square">7</button>
-                    <button className="square">8</button>
-                </div>
-                <div className="buttonrow">
-                    <button className="square">a</button>
-                    <button className="square">s</button>
-                    <button className="square">d</button>
-                    <button className="square">f</button>
-                    <button className="square">g</button>
-                    <button className="square">h</button>
-                    <button className="square">j</button>
-                    <button className="square">k</button>
-                </div>
-                <div className="buttonrow">
-                    <button className="square">31</button>
-                    <button className="square">32</button>
-                    <button className="square">33</button>
-                    <button className="square">34</button>
-                    <button className="square">35</button>
-                    <button className="square">36</button>
-                    <button className="square">37</button>
-                    <button className="square">38</button>
+                    <button arian-label="cowbell" onMouseDown={() => v({id: 'virgo2'})} className="square">6</button>
+                    <button arian-label="cowbell" onMouseDown={() => v({id: 'virgo3'})} className="square">7</button>
+                    <button arian-label="cowbell" onMouseDown={() => w({id: 'wave1'})} className="square">8</button>
+                    <button arian-label="cowbell" onMouseDown={() => w({id: 'wave2'})} className="square">9</button>
                 </div>
             </div>
         )
